d309d08e8705890019533a9a1ebf2e95b5b0264c
joonsol <71648638+joonsol@users.noreply.github.com>
2025-05-08 15:21:38 +0900

    1

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/new-test2/', 
+  build: {
+    outDir: 'docs',
+  },
+  plugins: [react()],
+
+})
